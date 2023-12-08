fe14ee71409cbac1d3e2b7264c8e5e03ebdf4523
Anurag <anuragkt3@gmail.com>
2023-12-09 00:15:46 +0530

    terraform_script

---
+++ EC2/variable.tf
@@ -0,0 +1,9 @@
+variable "ami_value" {
+  description = "value of AMI"
+}
+variable "instance_type_value" {
+  description = "value of Instance type"
+}
+variable "subnet_id_value" {
+  description = "value of subnet ID"
+}
